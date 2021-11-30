4e723d7ca96dc22c262fec4498949cb4339de278
Aksidion Kreimben <aksidion@kreimben.com>
2021-12-01 00:42:13 +0900

    update

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -1,30 +1,6 @@
-;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
-
-;; Place your private configuration here! Remember, you do not need to run 'doom
-;; sync' after modifying this file!
-
-
-;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets.
 (setq user-full-name "Aksidion Kreimben"
       user-mail-address "aksidion@kreimben.com")
 
-;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
-;; are the three important ones:
-;;
-;; + `doom-font'
-;; + `doom-variable-pitch-font'
-;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;;
-;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
-;; font string. You generally only need these two:
-;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
-;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
-
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
 (setq doom-theme 'doom-one)
 (setq doom-font (font-spec :family "D2Coding" :size 24)
       doom-variable-pitch-font (font-spec :family "D2Coding" :size 24)
@@ -36,50 +12,39 @@
   '(font-lock-comment-face :slant italic)
   '(font-lock-keyword-face :slant italic))
 
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
 
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type t)
-
-
-;; Here are some additional functions/macros that could help you configure Doom:
-;;
-;; - `load!' for loading external *.el files relative to this one
-;; - `use-package!' for configuring packages
-;; - `after!' for running code after a package has loaded
-;; - `add-load-path!' for adding directories to the `load-path', relative to
-;;   this file. Emacs searches the `load-path' when you load packages with
-;;   `require' or `use-package'.
-;; - `map!' for binding new keys
-;;
-;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
-;; This will open documentation for it, including demos of how they are used.
-;;
-;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
-;; they are implemented.
-
 
 ;; Enable minimap.
 (minimap-mode t)
 
-;; Enable treemacs at first.
-;;(treemacs-add-and-display-current-project t)
-
 ;; Related about lsp.
+(setq lsp-ui-mode t)
+
 (setq lsp-ui-doc-enable t)
 (setq lsp-ui-doc-delay 0)
 (setq lsp-ui-doc-show-with-cursor t)
 (setq lsp-ui-doc-show-with-mouse t)
+
 (setq lsp-treemacs-sync-mode 1)
+
 (setq lsp-completion-show-detail t)
 (setq lsp-completion-show-kind t)
+
+;(setq lsp-ui-sideline-enable 0)
+(setq lsp-ui-sideline-show-diagnostics t)
+(setq lsp-ui-sideline-show-hover t)
+(setq lsp-ui-sideline-show-code-actions t)
+(setq lsp-ui-sideline-update-mode 'line)
+(setq lsp-ui-sideline-delay 0)
 
 (global-visual-line-mode t)
 
 (use-package! lsp-tailwindcss)
 
+;; 전체화면 세팅
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
+
+;; Rust settings
+;(after! rustic (setq rustic-lsp-server 'rust-analyzer))
