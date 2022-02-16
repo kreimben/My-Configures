5bf9eb90943544abe2194663ce7a6af0388447a2
Aksidion Kreimben <aksidion@kreimben.com>
2022-02-16 22:54:06 +0900

    Add Fish Shell

---
+++ fish/config.fish
@@ -0,0 +1,9 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+alias bios="sudo systemctl reboot --firmware-setup"
+alias w="curl wttr.in/anseong"
+alias doom="~/.emacs.d/bin/doom"
+alias win="sudo grub-reboot 1 && sudo reboot now"
+alias vultr="ssh kreimben@141.164.55.41"
