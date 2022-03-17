59285e4758ad0ef306837ae5464111266003b094
Aksidion Kreimben <aksidion@kreimben.com>
2022-03-18 05:26:12 +0900

    Delete deno.

--- fish/config.fish
+++ fish/config.fish
@@ -8,6 +8,3 @@ alias doom="~/.emacs.d/bin/doom"
 alias win="sudo grub-reboot 1 && sudo reboot now"
 alias vultr="ssh root@149.28.209.214"
 alias lh="ls -hal"
-
-export DENO_INSTALL="/home/kreimben/.deno"
-export PATH="$DENO_INSTALL/bin:$PATH"
