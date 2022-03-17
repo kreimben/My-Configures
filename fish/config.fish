7db6f4667fac0b4689e9c856794d97b58d09f510
Aksidion Kreimben <aksidion@kreimben.com>
2022-03-18 04:05:33 +0900

    Update.

--- fish/config.fish
+++ fish/config.fish
@@ -7,3 +7,7 @@ alias w="curl wttr.in/anseong"
 alias doom="~/.emacs.d/bin/doom"
 alias win="sudo grub-reboot 1 && sudo reboot now"
 alias vultr="ssh root@149.28.209.214"
+alias lh="ls -hal"
+
+export DENO_INSTALL="/home/kreimben/.deno"
+export PATH="$DENO_INSTALL/bin:$PATH"
